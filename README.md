acd7d4fa3babd84ac9b03d211dddad67b8c58906
Dominic C <45153623+DominicChm@users.noreply.github.com>
2022-03-17 17:17:44 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Facilitates the updating and running of DAQ software on a Raspberry Pi.
+Install:
+
+```sudo apt install git```
+
+Install nvm
+
+`nvm use 17`
---
+++ install.sh
@@ -0,0 +1,25 @@
+sudo apt install git -y
+sudo apt-get install libudev-dev -y # Used for usb
+
+
+if ! command -v nvm &> /dev/null
+then
+  echo "Installing nvm..."
+  # Setup node.js
+  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+  nvm install 17
+  nvm use 17
+fi
+
+
+#Set up the service.
+sudo systemctl disable daq-bootstrap
+
+sudo cp ./daq-bootstrap.service /etc/systemd/system/
+sudo systemctl start daq-bootstrap
+sudo systemctl enable daq-bootstrap
