4763ac7b7c141f6630892f921aa42ac78301679b
Dominic C <45153623+DominicChm@users.noreply.github.com>
2022-03-18 16:14:30 -0700

    Add auto-cloning and NTFS enforcement.

--- index.js
+++ index.js
@@ -30,10 +30,27 @@ async function pollUSB() {
 
 async function onAttach(drive) {
     console.log("Connected... starting DAQ!");
+    
+    if(driveType(`${drive.device}1`) !== "ntfs")
+        throw new Error("ERROR - THIS DRIVE IS NOT NTFS FORMATTED!");
 
     execSync(`mount -o umask=0 ${drive.device}1 ./usb`);
-    execSync(`export NODE_VERSION=17.7.1`);
 
+    if (!fs.existsSync("./usb/bajacorev1"))
+        execSync("(cd ./usb; git clone https://github.com/DominicChm/bajacorev1.git)");
+
+    if (!fs.existsSync("./usb/bajafrontendv1"))
+        execSync("(cd ./usb; git clone https://github.com/DominicChm/bajafrontendv1.git)");
+
+    // Try to update the code, if we have a network connection.
+    try {
+        execSync("(cd ./usb/bajafrontendv1; git pull)");
+        execSync("(cd ./usb/bajacorev1; git pull)");
+    } catch (e) {
+        console.log("Couldn't update.");
+    }
+
+    console.log("Starting DAQ...")
     currentMount = drive.device;
     frontendProcess = execSync("(cd ./usb/bajafrontendv1; sudo -u pi npm i; sudo -u pi npm run start > frontend.log)");
     backendProcess = spawn("(cd ./usb/bajacorev1; sudo -u pi npm i; sudo -u pi npm run dev > backend.log)");
@@ -52,6 +69,10 @@ async function onDetach() {
     backendProcess = null;
 
     console.log("Killed DAQ!");
+}
+
+function driveType(fsLabel) {
+    return execSync(`sudo blkid -s TYPE -o value /dev/sdc1`).toString().trim();
 }
 
 setInterval(pollUSB, 1000);
