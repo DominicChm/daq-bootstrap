114de3d63457ebc09024c704ced2429837cbbe4f
Dominic C <45153623+DominicChm@users.noreply.github.com>
2022-03-18 23:24:20 -0700

    Update start-daq.sh

--- start-daq.sh
+++ start-daq.sh
@@ -1,7 +1,7 @@
-cd ./usb/daq-prebuilt/ || exit;
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
+cd ./usb/daq-prebuilt/dist || exit;
 echo "Starting..."
 export HTTP_PORT=80
 export DAQ_DATA_DIR="${parent_path}/usb"
-sudo n exec 17 npm run start
+sudo n exec 17 node index.js
